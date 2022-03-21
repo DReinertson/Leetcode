ce345eeaf787bd7c216fc8a4e0bf04cd28f96416
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-03-20 19:15:14 -0700

    Update Find_All_Numbers_Disappeared_In_Array.js

--- Arrays/Find_All_Numbers_Disappeared_In_Array.js
+++ Arrays/Find_All_Numbers_Disappeared_In_Array.js
@@ -3,44 +3,20 @@
  * @return {number[]}
  */
 
-//On to something here, closest to an Θ(n) solution so far.
-//Can use a for loop with indexOf in it, but that would be Θ(n^2)...
+//solution will be Θ(n^2)...
+//this solution doesn't work because it overwrites values that could be used to identify. This solution would work if I pushed the values to another array. 
 
 var findDisappearedNumbers = function(nums) {
     
-    let counter = 1;
-    let position = 0;
-    let x = 0;
+    let length = nums.length;
     
-    nums.sort(function(a,b){
-        return a-b;
-    })
-    
-    console.log('nums: ', nums);
-    
-    while (counter < nums.length){
-        console.log('counter: ', counter);
-        console.log('position: ', position);
-        console.log('x: ', x);
-        console.log('nums[x]: ', nums[x]);
-        if (x === 0) {
-            if (nums[x] === 1){
-                counter++;
-                x++;
-            } else{
-                x++;
-            }
-        } else{
-            if (nums[x] !== counter){
-                nums[position] = counter;
-                position++;
-                counter++;
-            } else{
-                x++;
-            }
+    for (let x = 1; x <= length; x++){
+        if (nums.indexOf(x) === -1){
+            nums.push(x);
         }
-        console.log('nums after: ', nums);
     }
+    
+    return nums.slice(length);
     
     
 };
