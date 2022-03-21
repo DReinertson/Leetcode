ebeb2364e30593046220e35234ee3f2052720f5c
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-03-20 21:51:59 -0700

    Update Rotate_Array.js

--- Easy Problem Set/Rotate_Array.js
+++ Easy Problem Set/Rotate_Array.js
@@ -4,14 +4,25 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 
-//Solution works, but times out on large data sets. Trying to make two arrays and concat them, but it won't return the proper array. 
+
+//Reverse the array 
+function reverse(arr, start, end){
+    while (start < end){
+        [arr[start], arr[end]] = [arr[end], arr[start]];
+        start++;
+        end--;
+    }
+}
+
+
 var rotate = function(nums, k) {
     
-   let count = 0;
+   let n = nums.length;
     
-    while(count < k){
-        nums.unshift(nums[nums.length-1]);
-        nums.pop();
-        count++;
-    }
+    k %= n
+    
+    //Reverse the array, then the first portion of the array (up to k), then the 2nd portion of the array
+    reverse(nums, 0, nums.length-1);
+    reverse(nums, 0, k-1);
+    reverse(nums, k, nums.length-1);
 };
