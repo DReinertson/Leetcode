e93809d4654460d4091465f88614fb756f5dd6e6
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-03-29 15:12:36 -0700

    Update Two_Sum.js
    More efficient answer. Just need to check if the key of value exists, if it doesn't set that key value pair to the map.

--- Easy Problem Set/Two_Sum.js
+++ Easy Problem Set/Two_Sum.js
@@ -6,31 +6,15 @@
 var twoSum = function(nums, target) {
     
     let numMap = new Map();
-    let finalArr = [];
-    let num1;
-    let targetNum;
     
     for (let x = 0; x < nums.length; x++){
-        numMap.set(x, nums[x]);      
-    }
-    
-    // console.log('numMap: ', numMap);
-    
-    let values = [...numMap.values()];
-    
-    for ([key, value] of numMap){
-        // console.log('key: ', key, 'value: ', value);
-        targetNum = target - value;
+        let remain = target - nums[x];
         
-        // console.log('targetNum: ', targetNum);
-        
-        
-        if (values.includes(targetNum) && key !== nums.indexOf(targetNum)){
-            finalArr.push(key);
-            finalArr.push(nums.indexOf(targetNum));
-            break;
+        if (numMap.get(remain) > -1 ){
+            return[x, numMap.get(remain)]
         }
+        
+        
+        numMap.set(nums[x], x);
     }
-    
-    return finalArr;
 };
