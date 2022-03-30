d0355f84de2aaf8107cf0903401d3f576b526328
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-03-30 08:47:26 -0700

    Update First_Unique_Character_In_String.js

--- Easy Problem Set/Strings/First_Unique_Character_In_String.js
+++ Easy Problem Set/Strings/First_Unique_Character_In_String.js
@@ -4,32 +4,12 @@
  */
 var firstUniqChar = function(s) {
     
-    let arr = s.split('');
-    console.log('arr: ', arr);
-    
-    let counter = new Map();
-    
-    for (let x = 0; x < arr.length; x++){
-        if (counter.has(arr[x])){
-            counter.set(arr[x], counter.get(arr[x]) - 1);
-        } else{
-            counter.set(arr[x], 1);
+    for (let x = 0; x < s.length; x++){        
+        if(s.indexOf(s[x]) === s.lastIndexOf(s[x])){
+            return x;
         }
-    }
-    
-    counter.forEach(function(value, key){
-        if (value <= 0){
-            counter.delete(key);
-        }
-    })
-    
-
-    console.log('map: ', counter);
-    if(counter.size === 0 ){
-        return -1
-    } else{
-        return arr.indexOf(counter.keys().next().value);
-    }
+    }    
+    return -1
     
     
     
