9dd32e878bf8bda6087bad2614444cfb6c0330d5
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-06-21 23:01:53 -0700

    Create Group anagram.js
    Unfinished

---
+++ NeetCode/Arrays & Hashing/Group anagram.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+//Will create a map with the key:value pair of the alphabatized string:actual word. For instance, strs[0] will get stored in the map as "aet" : "eat", strs[1] will get added to the "aet" key. Should I store the value as an array, will I be able to add to this array? How do you add to an array that's stored in a map? map.set(map.get(key).push(value))?
+
+var groupAnagrams = function(strs) {
+    let map1 = new Map();
+    for (let x in strs){
+        let sorted = strs[x].split('').sort().join('');
+        let arr = [];
+        arr.push(sorted);
+        console.log('sorted: ', sorted);
+        if (map1.get(sorted)){
+        }
+    }
+    
+};
