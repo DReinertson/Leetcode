44a36f8c74e881e774bb1eb4db087364ba07ef3a
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-06-21 21:33:18 -0700

    Create Valid_Anagram.js

---
+++ NeetCode/Arrays & Hashing/Valid_Anagram.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ 
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    
+    if (s.length !== t.length){
+        return false;
+    }
+    
+    let counter = new Map();
+    
+    for (let x = 0; x < s.length; x++){
+        if(counter.has(s[x])){
+            counter.set(s[x], counter.get(s[x]) + 1);
+        } else{
+            counter.set(s[x], 1);
+        }
+    }
+    
+    for (let y = 0; y < t.length; y++){
+        if(counter.has(t[y])){
+            counter.set(t[y], counter.get(t[y]) -1);
+            if(counter.get(t[y]) === 0){
+                counter.delete(t[y]);
+            }
+        } else{
+            counter.set(t[y], 1);
+        }
+    }
+    
+    if (counter.size === 0){
+        return true;
+    } 
+    
+    return false;
+};
