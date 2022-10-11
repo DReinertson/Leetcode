d78d492d8b551a741f295b350554602bbb3b455a
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-10-11 16:24:27 -0700

    Update Group Anagrams.js

--- NeetCode/Arrays & Hashing/Group Anagrams.js
+++ NeetCode/Arrays & Hashing/Group Anagrams.js
@@ -1,23 +1,38 @@
-Given an array of strings strs, group the anagrams together. You can return the answer in any order.
-
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-
 /**
  * @param {string[]} strs
  * @return {string[][]}
  */
 
-//Will create a map with the key:value pair of the alphabatized string:actual word. For instance, strs[0] will get stored in the map as "aet" : "eat", strs[1] will get added to the "aet" key. Should I store the value as an array, will I be able to add to this array? How do you add to an array that's stored in a map? map.set(map.get(key).push(value))?
+//How to tell if they are anagram? do they contain the same number of each letter?
+
+//Create map. Sort the strs[index]. Add first value to map. 
+//Loop through the array, does that key exist in the map? If so, add the strs[index] to that key, if not, create a key with that sorted strs[index]. 
 
 var groupAnagrams = function(strs) {
+    
+    let sort;
     let map1 = new Map();
+    
     for (let x in strs){
-        let sorted = strs[x].split('').sort().join('');
-        let arr = [];
-        arr.push(sorted);
-        console.log('sorted: ', sorted);
-        if (map1.get(sorted)){
+        sort = strs[x].split('').sort().join('');
+        if (x === 0){
+            map1.set(sort, []);
+            map1.get(sort).push(strs[x]);
+        }
+        else if (map1.has(sort)){
+            map1.get(sort).push(strs[x]);
+        }
+        else{
+            map1.set(sort, []);
+            map1.get(sort).push(strs[x]);
         }
     }
     
+    let arr = [...map1.values()];
+    
+    console.log(map1);
+    return arr;
 };
+
+Runtime: 306 ms, faster than 12.75% of JavaScript online submissions for Group Anagrams.
+Memory Usage: 62.4 MB, less than 5.12% of JavaScript online submissions for Group Anagrams.
