01c3c29df95c36d9f2c7e0e57e75084e1d50acc3
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-10-11 22:36:35 -0700

    Update top_k_frequent_elements.js

--- NeetCode/Arrays & Hashing/top_k_frequent_elements.js
+++ NeetCode/Arrays & Hashing/top_k_frequent_elements.js
@@ -6,7 +6,9 @@
 var topKFrequent = function(nums, k) {
     
     let map1 = new Map();
+    let finalArr = [];
     
+    //create map of each number in nums array with frequency of number. 
     for (let x in nums){
         if(map1.has(nums[x])){
             map1.set(nums[x], map1.get(nums[x]) + 1);
@@ -15,9 +17,24 @@ var topKFrequent = function(nums, k) {
         }
     }
     
-    console.log(map1);
-    let arr = [...map1.values()].sort();
-    console.log(arr);
+    // console.log(map1);
+    // let arr = [...map1.values()].sort();
+    // let arr = Array.from(map1, ([key, value]) => {return {[key]: value}}).sort();
+    // console.log(arr);
+    
+    let sortedMap = new Map([...map1].sort((a,b) => b[1] - a[1]));
+    
+    // console.log('sortedMap: ', sortedMap);
+    
+    for (let [key, value] of sortedMap){
+        if (finalArr.length !== k){
+            finalArr.push(key);
+        } else {
+            return finalArr;
+        }
+    }
+    
+    return finalArr;
+Runtime: 108 ms, faster than 73.66% of JavaScript online submissions for Top K Frequent Elements.
+Memory Usage: 46.8 MB, less than 34.18% of JavaScript online submissions for Top K Frequent Elements.
 };
-
-//Need to figure out how to get top k elements. Would be able to retrieve key with highest value, using Math.max, unsure how to get multiple keys. 
