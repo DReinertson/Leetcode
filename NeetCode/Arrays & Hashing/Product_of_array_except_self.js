fcd83b79b355bb3024c078e610ff041f51c3bb45
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-10-12 00:41:40 -0700

    Update Product_of_array_except_self.js

--- NeetCode/Arrays & Hashing/Product_of_array_except_self.js
+++ NeetCode/Arrays & Hashing/Product_of_array_except_self.js
@@ -3,61 +3,32 @@
  * @return {number[]}
  */
 
-//1. If there are more than one 0s in nums, the result is an array consisting of all 0.
-//2. If there is a single 0 in nums, then the result is an array consisting of all 0 except at the index where there was 0 in nums, which will contain product of rest of array.
-//3. If there's no 0 in nums, then the result is an array ans where ans[i] = prod / nums[i] (prod = product of all elements in nums).
+//Iterate over the array from left to right, recording the product of the numbers we've seen so far.
+//Iterate backwards from right to left, multiplying each element in res by the product of the numbers we've seen so far from right-to-left.
 
-//too many for loops. 
+
 var productExceptSelf = function(nums) {
     
-    let product;
     let finalArr = [];
-    let count = 0;
+    let product = 1;
     
-    if (nums.includes(0)){
-        let noZeros = nums.filter(function(val){
-            return val !== 0;
-        })
-        
-        if (noZeros.length === 0){
-            return nums;
-        }
-        product = noZeros.reduce(function(a,b){
-            return a*b;
-        })
-        
-        
-        //count the 0s
-        for (let y in nums){
-            if (nums[y] === 0){
-                count++;
-            }
-        }
-        
-        
-        for (let z in nums){
-            if (count > 1 || nums[z] !== 0){
-                finalArr[z] = 0;
-            } else{
-                finalArr[z] = product;
-            }
-        }
-    } else{
-        product = nums.reduce(function(a,b){
-            return a*b;
-        })
-        
-        for (let x in nums){
-            finalArr[x] = product / nums[x];
-        }
-        
-        
+    //Iterate from left to right    
+    for (let x in nums){
+        finalArr.push(product);
+        product *= nums[x];
     }
+    
+    //reinitialize product to 1, then iterate from right to left.
+    product = 1;
+    
+    for (let y = nums.length-1; y>=0; y--){
+        finalArr[y] *= product;
+        product *= nums[y];
+    }
+    
     return finalArr;
-    
-    
-    
+
 };
 
-Runtime: 236 ms, faster than 12.70% of JavaScript online submissions for Product of Array Except Self.
-Memory Usage: 61.6 MB, less than 5.19% of JavaScript online submissions for Product of Array Except Self.
+Runtime: 122 ms, faster than 86.48% of JavaScript online submissions for Product of Array Except Self.
+Memory Usage: 56.1 MB, less than 18.97% of JavaScript online submissions for Product of Array Except Self.
