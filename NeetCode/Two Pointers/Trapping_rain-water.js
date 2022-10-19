ef529dffe34897c4f19fb1935e860c5e7fe1ec97
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-10-19 00:13:16 -0700

    Update Trapping_rain-water.js

--- NeetCode/Two Pointers/Trapping_rain-water.js
+++ NeetCode/Two Pointers/Trapping_rain-water.js
@@ -2,73 +2,40 @@
  * @param {number[]} height
  * @return {number}
  */
-
-//How can you tell where water can be trapped? if the current index is x, water can be trapped if x-1 and x+1 are higher than x (if height[x-1] > height[x] < height[x+1]). Looking at the problem, this statement is false in a way. When you hit a peak, you continue from that point until you hit an equal or greater peak. 
-
-//For instance x = 1 is a peak and you count the squares until you hit an equal or greater peak, x = 3. But how do you count the squares? Let's look at the next section. 
-
-//Peak at x = 3. Continue on until you hit another peak, which is x=7. You can use the height of x=3 times the distance covered to get total area and then subtract the heights from the indices between. 
-
 var trap = function(height) {
     
-    let rain = 0;
+    let leftMax = -1; 
+    let rightMax = -1;
+    let water = 0; 
+    let left = 0; 
+    let right = height.length -1;
     
-    for (let x = 0; x < height.length; x++){
-        let diff;
-        let h;
-        let sum = 0;
-        let right = x+1;
-        let collection; 
-        let peak; 
-        let check = 0;
-        console.log('x: ', x);
-        console.log('height[x]: ', height[x]);
-        console.log('going into loop: ', height[x], height[x-1]);
-        console.log('----------------------------');
-        console.log('truth check: ', (x === 0 && height[x] > height[x+1]));
-        if ((height[x] > height[x-1]) || (x === 0 && height[x] > height[x+1])){
-            console.log('right: ', right);
-            console.log('height[right]: ', height[right]);
-            console.log('going into while loop: ', height[x], height[right]);
-            console.log('----------------------');
-            while(height[x] > height[right]){
-                peak = true; 
-                console.log('sum before: ', sum);
-                sum += height[right];
-                console.log('sum after: ', sum);
-                console.log('----------------');
-                console.log('Want to trigger peak false: ');
-                console.log('right + 1: ', right +1);
-                console.log('height length: ', height.length -1);
-                console.log('height[right+1]: ', height[right+1], height[x]);
-                if (right+1 === height.length-1 && height[right+1] < height[x]){
-                    peak = false;
-                    break;
-                }
-                console.log('right before adding: ', right);
-                right++;
-                console.log('right after adding: ', right);
-                console.log('-----------------------');
-                
-            }//End while loop
-            if (peak){
-                if (height[right] < height[x]){
-                    h = height[right];
-                } else {
-                    h = height[x];
-                }
-                diff = right - x - 1;
-                console.log('diff variables: ', right, x);
-                console.log('diff: ', diff);
-                console.log('-------------------------');
-                console.log('rain before: ', rain);
-                rain += diff * h - sum;
-                console.log('rain after: ', rain);
-            }
-            
-        } //End if height[x] > height[x-1]
-    } //End for loop
+    while (left <= right){
+        if (height[left] > leftMax){
+            leftMax = height[left];
+        } 
+        
+        if (height[right] > rightMax){
+            rightMax = height[right];
+        }
+        
+        console.log('leftMax: ', leftMax);
+        console.log("rightMax: ", rightMax);
+        console.log('water: ', water);
+        
+        
+        if (leftMax > rightMax){
+            water += rightMax - height[right];
+            right--
+        } else{
+            water += leftMax - height[left];
+            left++;
+        }
+    }
     
-    return rain;
+    return water;
     
 };
+
+Runtime: 66 ms, faster than 97.72% of JavaScript online submissions for Trapping Rain Water.
+Memory Usage: 42.6 MB, less than 92.83% of JavaScript online submissions for Trapping Rain Water.
