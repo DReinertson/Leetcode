9bebbc61f8e26adbc06cb024d1dda629aa06ce77
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-10-20 00:39:09 -0700

    Update Longest_substring_without_repeating_characters.js
    Worked on all but one test case. Error caused by print statements. 
    going to set the initial array to a set or map, to see if it exists in constant time. Should speed it up

--- NeetCode/Sliding Window/Longest_substring_without_repeating_characters.js
+++ NeetCode/Sliding Window/Longest_substring_without_repeating_characters.js
@@ -4,49 +4,55 @@
  */
 var lengthOfLongestSubstring = function(s) {
     
-    let map1 = new Map();
-    let count = 0;
-    let max = 1;
+    let tempArr = [];
+    let maxLength;
+    let left = 0;
+    let right = left + 1;
     
-    if (s === ''){
+    if (s.length === 0){
         return 0;
     }
     
-    for (let x = 0; x < s.length; x++){
-        console.log('s[x]: ', s[x], ' s[x-1]: ', s[x-1], ' s[x+1]: ', s[x+1]);
-        console.log(map1.has(s[x]));
-        if (s[x-1] === s[x+1]){
-            console.log('in equal if');
-            if (!map1.has(s[x])){
-                count+=1;                
+    tempArr.push(s[left]);
+    
+    maxLength = tempArr.length;
+    console.log('length: ', s.length);
+    
+    while (right < s.length){
+        console.log('in while loop left: ', left, ' right: ', right);
+        console.log('tempArr at beginning of loop: ', tempArr);
+        if (s[right] === s[right+1]){
+            console.log('sright = sright+1: ', s[right], ' ', s[right+1]);
+            if (tempArr.indexOf(s[right]) === -1){
+                tempArr.push(s[right]);                
             }
-            if (count > max){
-                max = count;
-            }            
-            map1 = new Map();            
-            count = 1;
-            map1.set(s[x], 1);
-        }
-        else if (!map1.has(s[x])){
-            console.log('adding count');
-            map1.set(s[x], 1);
-            count += 1;
-        }
-        else {
-            console.log('in else loop')
-            map1 = new Map();
-            if (count > max){
-                max = count;
+            left = right+1;
+            right = left+1;
+            if (tempArr.length > maxLength){
+                console.log('---------------');
+                console.log('in s[right] === s[right+1], changing max length');
+                console.log("maxLength before: ", maxLength, " tempArr: ", tempArr);
+                maxLength = tempArr.length;
             }
-            count = 1;
-            map1.set(s[x], 1);
+            tempArr = [s[left]];
         }
-        console.log('after loop: ', map1, count, max);
+        else if (tempArr.indexOf(s[right]) === -1){
+            tempArr.push(s[right]);
+            right++;
+        } else{
+            if (tempArr.length > maxLength){
+                maxLength = tempArr.length;
+            }
+            
+            tempArr.shift();
+            left++;
+        }
     }
     
-    if (count > max){
-        max = count;
+    if (tempArr.length > maxLength){
+        maxLength = tempArr.length;
     }
     
-    return max;
+    return maxLength;
+    
 };
