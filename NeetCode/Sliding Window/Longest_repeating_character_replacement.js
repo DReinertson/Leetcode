3f370494d31410b9c572cfd7cecabf13f2c7c249
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-10-20 21:48:15 -0700

    Update Longest_repeating_character_replacement.js
    Will upload script without console.log statements

--- NeetCode/Sliding Window/Longest_repeating_character_replacement.js
+++ NeetCode/Sliding Window/Longest_repeating_character_replacement.js
@@ -12,41 +12,55 @@ var characterReplacement = function(s, k) {
     
     
     let max = 0;
-    let tempLength = 0;
-    let letterChange = 0; 
     let left = 0;
-    let right = left+1;
-    let targetLetter;
+    let right = 0;
+    let mostFrequent = 0;
+    let map1 = new Map();
+    let length;
     
-    
-    
-    while(right < s.length){
-        targetLetter = s[left];
-        if (right === 1){
-            tempLength++;
-        }
-        console.log("in while: ", left, ' ', right);
-        //Need conditions that will move left + 1 and right + 1. 
-        //Conditions need to include a comparison to k, to track how many changes have occurred
-        
-        if (s[left] === s[right] && letterChange < k){
-            tempLength ++;
-            right ++;
-        } else if (s[left] === s[right] && letterChange === k) {
+    while (right < s.length){
+        // console.log("in while loop: ", left, '|', right);
+        // console.log('values: ', s[left], '|', s[right]);
+        // console.log('beginning of loop, mostFrequent: ', mostFrequent);
+        length = right-left + 1;
+        // console.log("length: ",  length);
+        // console.log('---------------------------');
+        if (map1.has(s[right])){
+            // console.log('adding to s[right]: ', s[right], '|', map1.get(s[right]));
+            map1.set(s[right], map1.get(s[right]) + 1);
             
-        } else if(s[left] !== s[right] && letterChange < k){
-            tempLength++;
-            right++;
-            letterChange++;
-        } else if(s[left] !== s[right] && letterChange === k){
-            left++;
+        } else{
+            map1.set(s[right], 1);
         }
-    }
-    
-    if (tempLength > max){
-        max = tempLength;
+        
+        if (map1.get(s[right]) > mostFrequent){
+            mostFrequent = map1.get(s[right]);
+        }
+        
+        // console.log('if window is valid: ', length, '|', mostFrequent, '|', k);        
+        if (length-mostFrequent <= k){
+            // console.log('max before: ', max);
+            max = length;
+            // console.log('max after: ', max);
+        } else {
+            map1.set(s[left], map1.get(s[left]) -1);
+            if (map1.get(s[left]) < map1.get(s[right])){
+                mostFrequent = map1.get(s[right]);
+            } else {
+                mostFrequent = map1.get(s[left]);
+            }
+            left++
+            
+        }
+        right++;
+        
+        
+        // console.log('*****************************************');
     }
     
     return max;
     
 };
+
+Runtime: 183 ms, faster than 32.98% of JavaScript online submissions for Longest Repeating Character Replacement.
+Memory Usage: 44.2 MB, less than 38.56% of JavaScript online submissions for Longest Repeating Character Replacement.
