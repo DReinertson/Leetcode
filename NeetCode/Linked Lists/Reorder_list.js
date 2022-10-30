e29ba098087a113c2516ce58246031862e66050d
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-10-30 01:08:57 -0700

    Update Reorder_list.js

--- NeetCode/Linked Lists/Reorder_list.js
+++ NeetCode/Linked Lists/Reorder_list.js
@@ -12,18 +12,38 @@
 
 //Traverse the list until the last node (current.next === null). will keep track of original head node (in these cases, it'll be 1) and say 1.next = to current
 var reorderList = function(head) {
-    let currentNode = head;
-    let previousNode = null;
-    let nextNode = currentNode.next;
-    let tempHead;
+    let slow = head;
+    let fast = head; 
     
-    while (currentNode !== null){
-        if (currentNode.next === null){
-            head.next = currentNode;
-            currentNode.next = nextNode;
-            currentNode = currentNode.next;
-        } else{
-            currentNode = currentNode.next;
-        }
+    while (fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
     }
-};
+    
+    let prev = null; 
+    let current = slow;
+    
+    //reverse the 2nd half of the LL.
+    while (current){
+        let temp = current.next; //temp = 3,4,5 || temp = 4,5
+        current.next = prev; //current.next = null || current.next = 3,4,5
+        prev = current; // prev = 3,4,5 || prev = 4,5
+        current = temp; // current = 3,4,5 || current = 4,5
+    }
+    
+    let first = head;
+    let second = prev;
+    
+    while(second.next){
+        let temp = first.next;
+        first.next = second;
+        first = temp;
+        
+        temp = second.next;
+        second.next = first;
+        second = temp;
+    }
+}
+
+Runtime: 176 ms, faster than 26.96% of JavaScript online submissions for Reorder List.
+Memory Usage: 50.3 MB, less than 32.63% of JavaScript online submissions for Reorder List.
