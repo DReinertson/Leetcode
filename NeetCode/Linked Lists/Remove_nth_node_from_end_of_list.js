fd467d996cc8ec2139e7eca61cdf3b04004414b7
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-10-30 09:44:21 -0700

    Update Remove_nth_node_from_end_of_list.js
    This works to remove the current nodes next value, but the question tells you to remove the node nth FROM THE END of the list. 
    
    Going to try to create an array from the linked list, remove the value at index end-n, then create a new linked list?

--- NeetCode/Linked Lists/Remove_nth_node_from_end_of_list.js
+++ NeetCode/Linked Lists/Remove_nth_node_from_end_of_list.js
@@ -12,18 +12,27 @@
  */
 
 //Traverse down list, having a counter increment as you proceed (start counter at 1?). When getting to n, remove node, and set previousNodes.next to current.next
+
+//how do you detect the node from the end of the list? 
 var removeNthFromEnd = function(head, n) {
     let currentNode = head;
     let previousNode = null;
     let nextNode = null;
     let counter = 0;
+    let remove;
     
     while (currentNode){
         if (counter === n){
-            let temp = currentNode.next;
-            previousNode.next = temp;
-            break;
+            remove = true;
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+            counter++;            
         } else {
+            if (remove){
+                let temp = currentNode.next;
+                previousNode.next = temp;
+                break;
+            }
             previousNode = currentNode;
             currentNode = currentNode.next;
             counter++;
