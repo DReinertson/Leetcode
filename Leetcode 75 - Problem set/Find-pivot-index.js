508a847b1b52e9ff534ea525d2668c658b5864e1
Darryn J. Reinertson <35150825+DReinertson@users.noreply.github.com>
2022-11-04 01:10:37 -0700

    Final Solution
    Made this way harder on myself, but it was a confusing prompt. 
    
    Didn't need 2 counters or nested for loops.

--- Leetcode 75 - Problem set/Find-pivot-index.js
+++ Leetcode 75 - Problem set/Find-pivot-index.js
@@ -3,41 +3,31 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-    let sumLeft = 0;
-    let sumRight = 0;
-    let left = 0; 
-    let right = nums.length - 1;
-    let counter = 0; 
+    
+    let sum = 0; 
+    let leftsum = 0;
 
-    while (left <= right){
-        console.log('left | right: ', left, ' ', right);
-        console.log('nums: ', nums[left], ' ', nums[right]);
-        console.log('sums: ', sumLeft, ' ', sumRight);
-
-        if (counter === 0){
-            if (nums[left] > nums[right]){
-                sumRight += nums[right];
-                right--;
-            } else {
-                sumLeft += nums[left];
-                left++;
-            }
-            counter++;
-        } else{
-            if (sumLeft === sumRight){
-            return left;
-        } else if (sumLeft < sumRight){
-            sumLeft += nums[left];
-            left++;
-        } else{
-            sumRight += nums[right];
-            right--;
-        }
-        }
-        
-
-        console.log('-----------------------------');
+    for (let x = 0; x < nums.length; x++){
+        sum += nums[x];
     }
+
+    for (let y = 0; y < nums.length; y++){
+        console.log("y: ", y);
+        console.log('nums[y]: ', nums[y]);
+        console.log('sum: ', sum);
+        console.log('leftsum: ', leftsum);
+        if (leftsum === (sum - leftsum - nums[y])){
+            return y;
+        } else {
+            leftsum += nums[y];
+        }
+
+        console.log('---------------------');
+    }
+
 
     return -1;
 };
+
+131ms runtime (top 51%)
+44mb memory (top 6%)
